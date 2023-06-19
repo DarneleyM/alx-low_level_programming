9585e9bbd4ad45cdf536e93ab2594b38d9a2d3e9
DarneleyM <darneleym@gmail.com>
2023-06-19 05:33:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #!/bin/bash
 #include <stdio.h>
-/**
+ /**
  * main - A program that prints a line with puts function
 n
  * Return: 0 (Success)
- */
+ */ 
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
